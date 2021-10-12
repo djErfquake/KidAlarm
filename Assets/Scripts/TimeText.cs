3f75119c8c689df3c594d405af17d0ef5b5eda9e
Calvin LeVally <calvinlevally@gmail.com>
2021-10-11 21:33:41 -0400

    initial commit

---
+++ Assets/Scripts/TimeText.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class TimeText : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public Image backgroundImage;
+    public AudioSource notificationAudio;
+
+    public AlarmTime alarmTime;
+    public Color alarmColor = Color.green;
+    private bool isAlarm = false;
+
+
+    public float updateSeconds = 5;
+    private float timeSinceLastUpdate = 999;
+
+
+    private void Update() {
+        timeSinceLastUpdate += Time.deltaTime;
+        if (timeSinceLastUpdate >= updateSeconds) {
+            UpdateClock();
+            if (!isAlarm) {
+                CheckForAlarm();
+            }
+            else {
+                isAlarm = false;
+            }
+            timeSinceLastUpdate = 0;
+        }
+
+        if (isAlarm) {
+            backgroundImage.color = Color.Lerp(Color.black, alarmColor, Mathf.Min(5, timeSinceLastUpdate/5));
+        }
+    }
+
+    private void UpdateClock() {
+        text.text = DateTime.Now.ToString("h:mm");
+    }
+
+    private void CheckForAlarm() {
+        DateTime now = DateTime.Now;
+        if (now.Hour == alarmTime.hour && now.Minute == alarmTime.minute) {
+            isAlarm = true;
+            notificationAudio.Play();
+        }
+    }
+
+
+    public void SetAlarm(int hour, int minute, bool isPM) {
+        alarmTime.hour = isPM ? hour + 12 : hour;
+        alarmTime.minute = minute;
+        isAlarm = false;
+    }
+}
+
+
+[System.Serializable]
+public class AlarmTime {
+
+    public int hour;
+    public int minute;
+
+    
+}
